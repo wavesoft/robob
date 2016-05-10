482b901b2dd4844dc1bc68f358dd905634ba08bc
Ioannis Charalampidis <icharala@pb-d-128-141-47-117.cern.ch>
2016-05-10 15:57:50 +0200

    Renaming robob.py to cli.py

--- setup.py
+++ setup.py
@@ -75,7 +75,7 @@ setup(
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
-            'robob=robob.robob:main',
+            'robob=robob.cli:main',
         ],
     },
 )
