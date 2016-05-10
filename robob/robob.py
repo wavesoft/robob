24d2a8abc3f4ca605f7a4553fa8be22ea9d8a5c7
Ioannis Charalampidis <icharala@pb-d-128-141-47-117.cern.ch>
2016-05-10 15:50:18 +0200

    Adding setuptools

---
+++ robob/robob.py
@@ -0,0 +1,100 @@
+import sys
+import time
+import signal
+import logging
+import robob.logger
+
+from robob.util import time2sec
+from robob.specs import Specs
+from robob.driver import TestDriver
+
+def help():
+	"""
+	Show help screen
+	"""
+	print("Use: run.sh <path-to-benchmark>")
+	sys.exit(1)
+
+def main():
+
+	# Show help screen if missing arguments
+	if len(sys.argv) < 2:
+		help()
+		return
+
+	# Get a logger
+	logger = logging.getLogger("robob")
+
+	# Load specs
+	specs = Specs( sys.argv[1] )
+	specs.load()
+
+	# Create test contexts
+	tests = specs.createTestContexts()
+
+	# Create reporter
+	reporter = specs.createReporter()
+	reporter.start()
+
+	# Gracefully shutdown
+	def cleanup(signal, frame):
+
+		# Interrupt reporter
+		logger.warn("Received break signal from the user")	
+
+		# Interrupt driver and reporter
+		driver.interrupt()
+		reporter.interrupt( driver.summarize() )
+		reporter.close()
+
+		# Exit with error
+		sys.exit(1)
+
+	# Trap shutdown signal
+	signal.signal(signal.SIGINT, cleanup)
+
+	# Create stream context for every test
+	driver = None
+	test_id = 0
+	for test in tests:
+
+		# Create a test driver
+		driver = TestDriver( specs, test )
+
+		# Get some values from test specs
+		iterations = int(test.get("test.iterations", 1))
+		cooldown = time2sec(test.get("test.cooldown", 0))
+
+		# Start reporting the test
+		reporter.test_start( test )
+
+		# Run multiple iterations of the test
+		for i in range( 0, iterations ):
+
+			# Calculate progress
+			p_total = (len(tests) * iterations)
+			p_curr = test_id * iterations + i
+
+			# Start log
+			logger.info("Running %i/%i (test: %i/%i, iteration: %i/%i, values: {%s})" % (p_curr+1, p_total, test_id+1, len(tests), i+1, iterations, \
+				", ".join([ "%s=\"%s\"" % (k, str(v)) for k,v in test['curr'].iteritems() ]) ))
+			reporter.iteration_start( i+1 )
+
+			# Run driver
+			driver.run(i)
+			reporter.iteration_end( driver.lastResults, driver.lastStatus, driver.lastComment )
+
+			# Apply cooldown
+			if cooldown:
+				logger.info("Waiting for %s sec before next test" % test.get("test.cooldown", "0"))
+				time.sleep(cooldown)
+
+		# Summarize iterations and finalize test
+		reporter.test_end( driver.summarize() )
+
+		# Increment test ID
+		test_id += 1
+
+	# Finalize reporter
+	reporter.finalize()
+	reporter.close()
---
+++ setup.py
@@ -0,0 +1,81 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='robob',
+    version='0.1.0b1',
+
+    description='A python tool for simplifying collection of measurements over repetitive tasks',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/wavesoft/robob',
+
+    # Author details
+    author='Ioannis Charalampidis',
+    author_email='johnys2@gmail.com',
+
+    # License
+    license='Apache-2.0',
+
+    # Classifiers
+    classifiers=[
+
+        # Project maturity
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: System Administrators',
+
+        # Topics
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Utilities',
+
+        # License
+        'License :: OSI Approved :: Apache Software License',
+
+        # Python versions (tested)
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='automation benchmark scripting test',
+
+    # Packages
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # Project dependencies
+    install_requires=['PyYAML'],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'robob=robob.robob:main',
+        ],
+    },
+)
